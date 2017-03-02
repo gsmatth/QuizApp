d47370a8777a9f15076a6b3b2029c1edcfb558f5
Doug Popadince <gsmatth@icloud.com>
2017-03-02 09:30:55 -0700

    added two additional questions and EditText views

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -25,14 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void scoreQuiz(View view) {
-        
+
         EditText textAnswerObject = (EditText) findViewById(R.id.question_1_answer);
         String textAnswerString = textAnswerObject.getText().toString();
         if (textAnswerString.equals(correctAnswerText[0])) {
             quantityOfCorrectAnswers += 1;
             Log.i("mainActivity", "value of correctAnswers: " + quantityOfCorrectAnswers);
         }
-        double quizScoreInPercent = (quantityOfCorrectAnswers / 8.00) * 100;
+
+        EditText textAnswerTwoObject = (EditText) findViewById(R.id.question_2_answer);
+        String textAnswerTwoString = textAnswerTwoObject.getText().toString();
+        if (textAnswerTwoString.equals(correctAnswerText[1])) {
+            quantityOfCorrectAnswers += 1;
+            Log.i("mainActivity", "value of correctAnswers: " + quantityOfCorrectAnswers);
+        }
+        EditText textAnswerThreeObject = (EditText) findViewById(R.id.question_3_answer);
+        String textAnswerThreeString = textAnswerThreeObject.getText().toString();
+        if (textAnswerThreeString.equals(correctAnswerText[2])) {
+            quantityOfCorrectAnswers += 1;
+            Log.i("mainActivity", "value of correctAnswers: " + quantityOfCorrectAnswers);
+        }
+
+        float quizScoreInPercent = (quantityOfCorrectAnswers / 8.00f) * 100;
 
         if (quizScoreInPercent >= 75) {
             Toast.makeText(this, "You have passed the quiz, with a score of " + quizScoreInPercent + "%", Toast.LENGTH_SHORT).show();
